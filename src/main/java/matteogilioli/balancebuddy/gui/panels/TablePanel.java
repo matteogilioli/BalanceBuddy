b29c3ce0cab1103ac7909de97d4704e03248dedc
Matteo Gilioli <55749040+matteogilioli@users.noreply.github.com>
2023-01-07 16:45:30 +0100

    Colonna Importo e Data allineate a destra

--- src/main/java/matteogilioli/balancebuddy/gui/panels/TablePanel.java
+++ src/main/java/matteogilioli/balancebuddy/gui/panels/TablePanel.java
@@ -1,18 +1,11 @@
 package matteogilioli.balancebuddy.gui.panels;
 
-import matteogilioli.balancebuddy.gui.components.BalanceTable;
 import matteogilioli.balancebuddy.gui.components.BalanceTableModel;
 import matteogilioli.balancebuddy.logic.BalanceEntry;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableColumnModel;
-import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public final class TablePanel extends JPanel {
@@ -23,7 +16,12 @@ public final class TablePanel extends JPanel {
         super();
 
         tableModel = new BalanceTableModel(entries);
-        table = new BalanceTable(tableModel);
+        table = new JTable(tableModel);
+        DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
+        rightRenderer.setHorizontalAlignment( JLabel.RIGHT );
+        table.getColumnModel().getColumn(1).setCellRenderer(rightRenderer);
+        table.getColumnModel().getColumn(2).setCellRenderer(rightRenderer);
+
         JScrollPane tableScrollPane = new JScrollPane(table);
 
         this.add(tableScrollPane, BorderLayout.CENTER);
