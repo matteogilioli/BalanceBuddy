b9b3bf58466a1e8a351562335d24b8cbbc58b095
Matteo Gilioli <55749040+matteogilioli@users.noreply.github.com>
2023-01-14 18:38:17 +0100

    Rimossi Errori/Warning

--- src/main/java/matteogilioli/balancebuddy/model/FilterType.java
+++ src/main/java/matteogilioli/balancebuddy/model/FilterType.java
@@ -1,13 +1,20 @@
 package matteogilioli.balancebuddy.model;
 
-import java.util.ArrayList;
-
 public enum FilterType {
     ALL(0), YEAR(1), MONTH(2), WEEK(3), DAY(4), CUSTOM(5);
 
-    FilterType(int i) { }
+    private final int index;
 
-    public static FilterType get(int i) {
-        return values()[i];
+    FilterType(int index) {
+        this.index = index;
+    }
+
+    public static FilterType get(int index) {
+        for (FilterType filterType : values()) {
+            if (filterType.index == index) {
+                return filterType;
+            }
+        }
+        throw new IllegalArgumentException("Invalid index: " + index);
     }
 }
